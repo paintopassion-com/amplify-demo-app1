1c0bd5a0788266bacbdd600924c8f0be31a4e774
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-08-22 15:13:25 +0000

    added the quiz code

--- amplify-demo-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify-demo-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifydemoapp04a50bae": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
